b273e98f7ef0f66080464f07ea88d698838d8975
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-02-07 02:41:05 -0300

    add nvim

--- nix/flake.nix
+++ nix/flake.nix
@@ -14,7 +14,7 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [ pkgs.neovim
         ];
 
       # services.nix-daemon.enable = true;
